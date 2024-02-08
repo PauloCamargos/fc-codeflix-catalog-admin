d0a3a14b266c5388609d2aa405ce9f288f17a20f
Paulo Camargos <camargosspaulo@gmail.com>
2024-02-07 22:31:01 -0300

    added mypy setup

---
+++ utils/dev-lint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_FILE_PATH="$(realpath "${BASH_SOURCE[0]}")"
+SCRIPT_FILE_DIRECTORY_PATH="$(dirname "${SCRIPT_FILE_PATH}")"
+BASE_PROJECT_PATH="$(dirname "${SCRIPT_FILE_DIRECTORY_PATH}")"
+
+MYPY_FILE_CONFIG="${BASE_PROJECT_PATH}/setup.cfg"
+BASE_SOURCE_CODE="${BASE_PROJECT_PATH}/src"
+
+cd $BASE_SOURCE_CODE
+
+echo "Running flake8"
+flake8 .
+
+echo "Running mypy"
+mypy --config-file="${MYPY_FILE_CONFIG}" .
