74699db36bc3c85ef45b6e55b9c460cc5c751d51
Paulo Camargos <camargosspaulo@gmail.com>
2024-02-11 15:36:32 -0300

    replace test base with pytest

--- src/django_project/category_app/tests/test_category_api.py
+++ src/django_project/category_app/tests/test_category_api.py
@@ -1,44 +1,59 @@
+import pytest
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
 
 from core.category.domain.category import Category
 from django_project.category_app.repository import DjangoORMCategoryRepository
 
 
-class TestCactegoryAPI(APITestCase):
-    def test_list_categories(self):
-        movie = Category(
+@pytest.mark.django_db
+class TestCactegoryAPI:
+    @pytest.fixture
+    def category_movie(self) -> Category:
+        return Category(
             name="Movie",
             description="Movie description",
             is_active=False,
         )
-        serie = Category(
+
+    @pytest.fixture
+    def category_serie(self) -> Category:
+        return Category(
             name="Serie",
             description="Serie description",
             is_active=False,
         )
 
-        repository = DjangoORMCategoryRepository()
-        repository.save(movie)
-        repository.save(serie)
+    @pytest.fixture
+    def category_repository(self) -> DjangoORMCategoryRepository:
+        return DjangoORMCategoryRepository()
+
+    def test_list_categories(
+        self,
+        category_movie: Category,
+        category_serie: Category,
+        category_repository: DjangoORMCategoryRepository,
+    ):
+        category_repository.save(category_movie)
+        category_repository.save(category_serie)
 
         expected_data = [
             {
-                "id": str(movie.id),
-                "name": movie.name,
-                "description": movie.description,
-                "is_active": movie.is_active,
+                "id": str(category_movie.id),
+                "name": category_movie.name,
+                "description": category_movie.description,
+                "is_active": category_movie.is_active,
             },
             {
-                "id": str(serie.id),
-                "name": serie.name,
-                "description": serie.description,
-                "is_active": serie.is_active,
+                "id": str(category_serie.id),
+                "name": category_serie.name,
+                "description": category_serie.description,
+                "is_active": category_serie.is_active,
             },
         ]
 
         url = "/api/categories/"
-        response = self.client.get(url)
+        response = APIClient().get(url)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertCountEqual(response.data, expected_data)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == expected_data
