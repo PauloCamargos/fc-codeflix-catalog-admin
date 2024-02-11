a0e7931c194bec1be0583eb636fc92267aea3e79
Paulo Camargos <camargosspaulo@gmail.com>
2024-02-11 11:50:35 -0300

    setup django category model and admin portal

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # fc-catalog-management
 Codeflix Catalog Management 
+
+# Utils
+- Entering database shell:
+  - `$ manage.py dbshell`
+  - You may need to install a client for your db. Exemple for `sqlite3`:
+    - `$ sudo apt-get install sqlite3 libsqlite3-dev`
--- src/django_project/category_app/models.py
+++ src/django_project/category_app/models.py
@@ -1,3 +1,17 @@
+from uuid import uuid4
+
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    class Meta:
+        db_table = "category"
+        verbose_name_plural = "categories"
+
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.name
