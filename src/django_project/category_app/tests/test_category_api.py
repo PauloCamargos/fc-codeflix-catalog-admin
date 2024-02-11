ccf4753c1f72c6cae9f5d8607dfefd384a5190a0
Paulo Camargos <camargosspaulo@gmail.com>
2024-02-11 15:28:26 -0300

    test category api with repository

--- src/django_project/category_app/tests/test_category_api.py
+++ src/django_project/category_app/tests/test_category_api.py
@@ -1,26 +1,44 @@
-from rest_framework.test import APITestCase
 from rest_framework import status
+from rest_framework.test import APITestCase
+
+from core.category.domain.category import Category
+from django_project.category_app.repository import DjangoORMCategoryRepository
 
 
 class TestCactegoryAPI(APITestCase):
     def test_list_categories(self):
-        url = "/api/categories/"
-        response = self.client.get(url)
+        movie = Category(
+            name="Movie",
+            description="Movie description",
+            is_active=False,
+        )
+        serie = Category(
+            name="Serie",
+            description="Serie description",
+            is_active=False,
+        )
+
+        repository = DjangoORMCategoryRepository()
+        repository.save(movie)
+        repository.save(serie)
 
         expected_data = [
             {
-                "id": "1",
-                "name": "Movie",
-                "description": "Movie description",
-                "is_active": True,
+                "id": str(movie.id),
+                "name": movie.name,
+                "description": movie.description,
+                "is_active": movie.is_active,
             },
             {
-                "id": "2",
-                "name": "Serie",
-                "description": "Serie description",
-                "is_active": False,
+                "id": str(serie.id),
+                "name": serie.name,
+                "description": serie.description,
+                "is_active": serie.is_active,
             },
         ]
 
+        url = "/api/categories/"
+        response = self.client.get(url)
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data, expected_data)
+        self.assertCountEqual(response.data, expected_data)
--- src/django_project/category_app/views.py
+++ src/django_project/category_app/views.py
@@ -1,24 +1,30 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.request import Request
+from rest_framework import status
+
+from core.category.application.list_categories import ListCategories, ListCategoryInput
+from django_project.category_app.repository import DjangoORMCategoryRepository
 
 
 class CategoryViewSet(viewsets.ViewSet):
     def list(self, request: Request) -> Response:
+        input = ListCategoryInput()
+        use_case = ListCategories(repository=DjangoORMCategoryRepository())
+
+        output = use_case.execute(input=input)
+
+        serialized_categories = [
+            {
+                "id": str(category.id),
+                "name": category.name,
+                "description": category.description,
+                "is_active": category.is_active,
+            }
+            for category in output.data
+        ]
+
         return Response(
-            status=200,
-            data=[
-                {
-                    "id": "1",
-                    "name": "Movie",
-                    "description": "Movie description",
-                    "is_active": True,
-                },
-                {
-                    "id": "2",
-                    "name": "Serie",
-                    "description": "Serie description",
-                    "is_active": False,
-                },
-            ],
+            status=status.HTTP_200_OK,
+            data=serialized_categories,
         )
