9ebad4a3942697751bf724c99dd445fbbcb8cac1
Paulo Camargos <camargosspaulo@gmail.com>
2024-02-13 14:37:50 -0300

    added more django repository tests

--- src/django_project/genre_app/repository.py
+++ src/django_project/genre_app/repository.py
@@ -21,13 +21,53 @@ class DjangoORMGenreRepository(AbstractGenreRepository):
             genre_model.categories.set(genre.categories)
 
     def get_by_id(self, id: UUID) -> Genre | None:
-        raise NotImplementedError
+        try:
+            genre_model = (
+                self.genre_model.objects
+                .prefetch_related("categories")
+                .get(id=id)
+            )
+        except self.genre_model.DoesNotExist:
+            return None
+
+        return Genre(
+            id=genre_model.id,
+            name=genre_model.name,
+            is_active=genre_model.is_active,
+            categories=set(
+                category.id
+                for category in genre_model.categories.all()
+            )
+        )
 
     def list_genres(self) -> list[Genre]:
-        raise NotImplementedError
+        genre_models = self.genre_model.objects.all()
+
+        return [
+            Genre(
+                id=genre_model.id,
+                name=genre_model.name,
+                is_active=genre_model.is_active,
+                categories=set(
+                    category.id
+                    for category in genre_model.categories.only("id")
+                )
+            )
+            for genre_model in genre_models
+        ]
 
     def delete(self, id: UUID) -> None:
-        raise NotImplementedError
+        self.genre_model.objects.filter(id=id).delete()
 
     def update(self, genre: Genre) -> None:
-        raise NotImplementedError
+        try:
+            genre_model = self.genre_model.objects.get(id=genre.id)
+        except self.genre_model.DoesNotExist:
+            return None
+
+        with transaction.atomic():
+            self.genre_model.objects.filter(id=genre.id).update(
+                name=genre.name,
+                is_active=genre.is_active,
+            )
+            genre_model.categories.set(genre.categories)
--- src/django_project/genre_app/tests/test_django_orm_genre_repository.py
+++ src/django_project/genre_app/tests/test_django_orm_genre_repository.py
@@ -1,8 +1,10 @@
+from uuid import uuid4
+
 import pytest
+
 from src.core.category.domain.category import Category
 from src.core.genre.domain.genre import Genre
 from src.django_project.category_app.repository import DjangoORMCategoryRepository
-
 from src.django_project.genre_app.repository import DjangoORMGenreRepository
 
 
@@ -18,12 +20,30 @@ def documentary_category() -> Category:
 
 @pytest.fixture
 def romance_genre() -> Genre:
-    return Genre(name="Action")
+    return Genre(name="Romance")
 
 
 @pytest.fixture
 def genre_repository() -> DjangoORMGenreRepository:
     return DjangoORMGenreRepository()
+
+
+@pytest.fixture
+def genre_repository_with_romance_genre(
+    romance_genre: Genre,
+    movie_category: Category,
+    documentary_category: Category,
+    category_repository: DjangoORMCategoryRepository,
+    genre_repository: DjangoORMGenreRepository,
+) -> DjangoORMGenreRepository:
+    romance_genre.add_category(movie_category.id)
+    romance_genre.add_category(documentary_category.id)
+
+    category_repository.save(category=movie_category)
+    category_repository.save(category=documentary_category)
+
+    genre_repository.save(genre=romance_genre)
+    return genre_repository
 
 
 @pytest.fixture
@@ -84,3 +104,91 @@ class TestSave:
                 for category in created_genre.categories.all()
             ) == romance_genre.categories
         )
+
+
+@pytest.mark.django_db
+class TestList:
+    def test_list_genre_success(
+        self,
+        romance_genre: Genre,
+        genre_repository_with_romance_genre: DjangoORMGenreRepository,
+    ):
+        genres = genre_repository_with_romance_genre.list_genres()
+
+        assert genres == [romance_genre]
+
+    def test_emtpy_list_genre_sucess(
+        self,
+        genre_repository: DjangoORMGenreRepository,
+    ):
+        genres = genre_repository.list_genres()
+        assert genres == []
+
+
+@pytest.mark.django_db
+class TestGetById:
+    def test_get_by_id_returns_none_success(
+        self,
+        genre_repository: DjangoORMGenreRepository,
+    ):
+        genre = genre_repository.get_by_id(id=uuid4())
+        assert genre is None
+
+    def test_get_by_id_success(
+        self,
+        romance_genre: Genre,
+        genre_repository_with_romance_genre: DjangoORMGenreRepository,
+    ):
+        genre_found = genre_repository_with_romance_genre.get_by_id(id=romance_genre.id)
+
+        assert genre_found is not None
+        assert genre_found.name == romance_genre.name
+        assert genre_found.is_active == romance_genre.is_active
+
+        assert genre_found.categories == romance_genre.categories
+
+
+@pytest.mark.django_db
+class TestUpdate:
+    def test_update_success(
+        self,
+        movie_category: Category,
+        romance_genre: Genre,
+        genre_repository_with_romance_genre: DjangoORMGenreRepository,
+    ):
+        new_name = "Love"
+        romance_genre.update_name(name=new_name)
+
+        if romance_genre.is_active:
+            romance_genre.deactivate()
+            new_is_active = False
+        else:
+            romance_genre.activate()
+            new_is_active = True
+
+        romance_genre.remove_category(id=movie_category.id)
+        new_categories = romance_genre.categories
+
+        genre_repository_with_romance_genre.update(genre=romance_genre)
+
+        genre_found = genre_repository_with_romance_genre.get_by_id(id=romance_genre.id)
+
+        assert genre_found is not None
+        assert genre_found.name == new_name
+        assert genre_found.is_active == new_is_active
+
+        assert genre_found.categories == new_categories
+
+
+@pytest.mark.django_db
+class TestDelete:
+    def test_delete_genre_success(
+        self,
+        romance_genre: Genre,
+        genre_repository_with_romance_genre: DjangoORMGenreRepository,
+    ):
+        genre_repository_with_romance_genre.delete(id=romance_genre.id)
+
+        genre_found = genre_repository_with_romance_genre.get_by_id(id=romance_genre.id)
+
+        assert genre_found is None
