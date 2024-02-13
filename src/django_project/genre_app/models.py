c3b3d7af9b169c06f177ddc7663ae98b05b04998
Paulo Camargos <camargosspaulo@gmail.com>
2024-02-13 11:46:48 -0300

    added genre model

--- src/django_project/genre_app/models.py
+++ src/django_project/genre_app/models.py
@@ -1,3 +1,21 @@
+from uuid import uuid4
+
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    class Meta:
+        app_label = "genre_app"
+        db_table = "genre"
+        verbose_name_plural = "genres"
+
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    categories = models.ManyToManyField(
+        to="category_app.Category",
+        related_name="genres",
+    )
+
+    def __str__(self) -> str:
+        return self.name
