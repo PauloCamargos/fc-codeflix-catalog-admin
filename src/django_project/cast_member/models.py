8bdc1eed0021919487559ce5fdb796bb9d327037
Paulo Camargos <camargosspaulo@gmail.com>
2024-02-16 17:38:52 -0300

    feat(django_app/models): create model cast member + migration

---
+++ src/django_project/cast_member/models.py
@@ -0,0 +1,26 @@
+from uuid import uuid4
+
+from django.db import models
+
+from src.core.cast_member.domain.cast_member import CastMemberType
+
+
+def cast_member_choices() -> dict[str, str]:
+    return {
+        str(cast_member_type): str(cast_member_type)
+        for cast_member_type in CastMemberType
+    }
+
+
+class CastMember(models.Model):
+    class Meta:
+        app_label = "cast_member"
+        db_table = "cast_member"
+        verbose_name_plural = "cast_members"
+
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=255, choices=cast_member_choices)
+
+    def __str__(self) -> str:
+        return self.name
