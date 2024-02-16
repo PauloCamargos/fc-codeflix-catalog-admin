96b59e2eca1cad00f0b16ef634fb574b9f626d00
Paulo Camargos <camargosspaulo@gmail.com>
2024-02-16 14:15:15 -0300

    refactor(application): split tests into separate files

---
+++ src/core/cast_member/tests/application/unit/test_delete_cast_member.py
@@ -0,0 +1,6 @@
+class TestDeleteCastMember:
+    def test_delete_cast_member_success(self):
+        raise NotImplementedError()
+
+    def test_delete_cast_member_does_not_exist_error(self):
+        raise NotImplementedError()
---
+++ src/core/cast_member/tests/application/unit/test_update_cast_member.py
@@ -0,0 +1,15 @@
+class TestUpdateCastMember:
+    def test_update_cast_member_success(self):
+        raise NotImplementedError()
+
+    def test_update_cast_member_invalid_name_length_error(self):
+        raise NotImplementedError()
+
+    def test_update_cast_member_invalid_name_content_error(self):
+        raise NotImplementedError()
+
+    def test_update_cast_member_invalid_type_error(self):
+        raise NotImplementedError()
+
+    def test_update_cast_member_does_not_exist_error(self):
+        raise NotImplementedError()
