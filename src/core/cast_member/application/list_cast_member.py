400222b01f76fc448157a0e05175fb31e862a2a6
Paulo Camargos <camargosspaulo@gmail.com>
2024-02-16 14:09:14 -0300

    test(application): add test to list cast member

---
+++ src/core/cast_member/application/list_cast_member.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from uuid import UUID
+
+from src.core.cast_member.gateway.cast_member_gateway import (
+    AbstractCastMemberRepository,
+)
+
+
+class ListCastMember:
+    @dataclass
+    class Input:
+        pass
+
+    @dataclass 
+    class Output:
+        id: UUID
+        name: str
+        type: str
+
+    def __init__(self, repository: AbstractCastMemberRepository):
+        self.repository = repository
+
+    def execute(self, input: Input) -> list[Output]:
+        cast_members = self.repository.list()
+
+        return [
+            ListCastMember.Output(
+                id=cast_member.id,
+                name=cast_member.name,
+                type=cast_member.type,
+            )
+            for cast_member in cast_members
+        ]
--- src/core/cast_member/tests/application/unit/test_create_cast_member.py
+++ src/core/cast_member/tests/application/unit/test_create_cast_member.py
@@ -4,6 +4,7 @@ import pytest
 
 from src.core.cast_member.application.create_cast_member import CreateCastMember
 from src.core.cast_member.application.errors import InvalidCastMemberData
+from src.core.cast_member.application.list_cast_member import ListCastMember
 from src.core.cast_member.domain.cast_member import (
     MAX_CAST_MEMBER_NAME_NUM_CHARACTERS,
     CastMember,
@@ -12,6 +13,11 @@ from src.core.cast_member.domain.cast_member import (
 from src.core.cast_member.gateway.cast_member_gateway import (
     AbstractCastMemberRepository,
 )
+
+
+@pytest.fixture
+def actor_cast_member() -> CastMember:
+    return CastMember(name="John", type="ACTOR")
 
 
 @pytest.fixture
@@ -94,3 +100,51 @@ class TestCreateCastMember:
                 type=input.type,
             ),
         )
+
+
+class TestListCastMember:
+    def test_list_cast_member_success(
+        self,
+        actor_cast_member: CastMember,
+        mocked_cast_member_repository: MagicMock,
+    ):
+        mocked_cast_member_repository.list.return_value = [actor_cast_member]
+
+        input = ListCastMember.Input()
+
+        use_case = ListCastMember(repository=mocked_cast_member_repository)
+
+        output = use_case.execute(input=input)
+
+        mocked_cast_member_repository.list.assert_called_once_with()
+
+        assert len(output) == 1
+        [found_cast_member_output] = output
+        assert found_cast_member_output.id == actor_cast_member.id
+        assert found_cast_member_output.name == actor_cast_member.name
+        assert found_cast_member_output.type == actor_cast_member.type
+
+
+class TestUpdateCastMember:
+    def test_update_cast_member_success(self):
+        raise NotImplementedError()
+
+    def test_update_cast_member_invalid_name_length_error(self):
+        raise NotImplementedError()
+
+    def test_update_cast_member_invalid_name_content_error(self):
+        raise NotImplementedError()
+
+    def test_update_cast_member_invalid_type_error(self):
+        raise NotImplementedError()
+
+    def test_update_cast_member_does_not_exist_error(self):
+        raise NotImplementedError()
+
+
+class TestDeleteCastMember:
+    def test_delete_cast_member_success(self):
+        raise NotImplementedError()
+
+    def test_delete_cast_member_does_not_exist_error(self):
+        raise NotImplementedError()
