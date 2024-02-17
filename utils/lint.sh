7dd7c369626de3b5f66680911c9436a9fb9cbef8
Paulo Camargos <camargosspaulo@gmail.com>
2024-02-16 23:13:21 -0300

    chore: update lint to check pending migrations

--- utils/lint.sh
+++ utils/lint.sh
@@ -9,10 +9,13 @@ BASE_PROJECT_PATH="$(dirname "${SCRIPT_FILE_DIRECTORY_PATH}")"
 MYPY_FILE_CONFIG="${BASE_PROJECT_PATH}/setup.cfg"
 BASE_SOURCE_CODE="${BASE_PROJECT_PATH}/src"
 
+echo "Checking pending migrations..."
+./manage.py makemigrations --check
+
 cd $BASE_SOURCE_CODE
 
-echo "Running flake8"
+echo "Running flake8..."
 flake8 .
 
-echo "Running mypy"
+echo "Running mypy..."
 mypy --config-file="${MYPY_FILE_CONFIG}" .
